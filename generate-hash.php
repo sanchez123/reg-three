9e9084b36d01e16c6c6bb5ea5feffb634e732a31
Brian Mose <wosukila@gmail.com>
2026-05-30 10:54:25 +0300

    Removed  first files

--- generate-hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$password = "admin123";
-$hash = password_hash($password, PASSWORD_BCRYPT);
-echo "Bcrypt Hash for 'admin123':<br>";
-echo "<strong>" . $hash . "</strong><br><br>";
-echo "Copy this hash and paste it in phpMyAdmin";
-?>
